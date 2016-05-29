bd8d0c64157c5d6e5d5563458cfe980c9ece9d29
Stephen Biston <sjb9774@gmail.com>
2016-05-29 06:26:19 -0500

    Start fleshing out app

--- js/handlers.js
+++ js/handlers.js
@@ -1,10 +1,22 @@
-var Handler = function() {
-  this.render = function(htmlPath) {
-    return function(request, response) {
-      response.sendFile('./views/' + htmlPath, {'root': './'});
-    }
-  };
-  return this;
-}
+var qs = require('querystring');
+var req = require('request');
 
+var Handler = function() {}
+
+Handler.prototype.render = function(htmlPath) {
+  return function(request, response) {
+    console.log("Serving " + htmlPath);
+    response.sendFile('./views/' + htmlPath, {'root': './'});
+  }
+};
+
+Handler.prototype.getJSON = function(request, response, next) {
+  var apiUrl = "http://127.0.0.1:9000";
+  var path = request.path;
+  var queryArgs = qs.stringify(request.query);
+  var fullUrl = apiUrl + path + (queryArgs ? '?' + queryArgs : '');
+  req(fullUrl, function(err, resp, body) {
+    response.set({"Content-type": "application/json"}).send(body);
+  });
+}
 module.exports = Handler;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
-var handler = new require('./js/handlers.js')();
+var handler = new require('./js/handlers.js');
+var myHandler = new handler();
 
-app.get('/', handler.render('index.html'));
+app.get('/', myHandler.render('index.html'));
+app.get('/api/:path', myHandler.getJSON);
 
 app.use(express.static('./views'));
+app.use('/views/js/', express.static(__dirname + '/views/js'));
+app.use('/views/css/', express.static(__dirname + '/views/css'));
 
 app.listen(8000, function() {
   console.log("Server listening on 127.0.0.1:8000");
